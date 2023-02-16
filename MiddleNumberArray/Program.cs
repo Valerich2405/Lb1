5f16cd03039ceba8b16bbc3e28e07e3b531f7213
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2023-02-17 00:47:49 +0200

    Add MiddleNumberArray

---
+++ MiddleNumberArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MiddleNumberArray
+{
+    class Program
+    {
+        delegate int MyDelegate();
+        delegate double MidCalc(MyDelegate[] a);
+
+        static int Randomize()
+        {
+            Random random = new Random();
+            int number = random.Next(1, 10);
+            Console.WriteLine(number);
+            return number;
+        }
+
+        static void Main()
+        {
+            Console.Write("Введiть кiлькiсть елементiв масива: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Згенерований масив:");
+            var array = new MyDelegate[x];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = () => new MyDelegate(Randomize)();
+            }
+
+            MidCalc calc = delegate (MyDelegate[] a)
+            {
+                double sr = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    sr += a[i]();
+                }
+                return sr / array.Length;
+            };
+
+            Console.WriteLine($"Середнє арифметичне значення = " + calc(array));
+            Console.WriteLine();
+
+        }
+    }
+}
