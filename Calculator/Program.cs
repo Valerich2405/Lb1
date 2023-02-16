5183dffc59e89532b1319a2d7eba68075514c3d8
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2023-02-16 18:32:24 +0200

    Add Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Calculator
+{
+    public delegate double Delegat(double a, double b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть перше число: ");
+            double a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введiть дiю (+,-,*,/): ");
+            string x = Console.ReadLine();
+
+            Console.Write("Введiть друге число: ");
+            double b = Convert.ToInt32(Console.ReadLine());
+
+            switch (x)
+            {
+                case "+":
+                    Delegat Add = (c, d) => c + d;
+                    double del1 = Add(a, b);
+                    Console.WriteLine("Результат: {0}", del1);
+                    break;
+
+                case "-":
+                    Delegat Sub = (c, d) => c - d;
+                    double del2 = Sub(a, b);
+                    Console.WriteLine("Результат: {0}", del2);
+                    break;
+
+                case "*":
+                    Delegat Mul = (c, d) => c * d;
+                    double del3 = Mul(a, b);
+                    Console.WriteLine("Результат: {0}", del3);
+                    break;
+
+                case "/":     
+                    Delegat Div = (c, d) => d == 0 ? 0 : c / d;
+                    double del4 = Div(a, b);
+                    Console.WriteLine("Результат: {0}", del4);
+                    break;
+
+                default:
+                    Console.WriteLine("Введiть доступну дiю (+,-,*,/).");
+                    break;
+            }
+        }
+    }
+}
