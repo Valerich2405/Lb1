217bdd2f772a93a68f56d7449a92c4d5a7703fb4
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2023-02-17 01:02:12 +0200

    Add MiddleNumber

---
+++ MiddleNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MiddleNumber
+{
+    class Program
+    {
+        static void MidCalc(int a, int b, int c)
+        {
+            Console.WriteLine($"Результат: " + ((a+b+c)/3));
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть перше число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть друге число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть третє число: ");
+            int c = int.Parse(Console.ReadLine());
+
+            MidCalc(a, b, c);
+        }
+    }
+}
