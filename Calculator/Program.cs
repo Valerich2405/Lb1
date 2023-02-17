0b471a8d0379b5fc31b52b23f434719bdf211890
Stanislav <S.Zelyunko_FIT_2_21_B_d@knute.edu.ua>
2023-02-17 13:42:20 +0200

    Optimize Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,11 +3,19 @@
 namespace Calculator
 {
     public delegate double Delegat(double a, double b);
+    public delegate void Action();
+    public delegate void Action<in T>(T obj);
 
     class Program
     {
         static void Main(string[] args)
         {
+            void DoCalculation(double a, double b, Action<double, double> op) => op(a, b);
+            void Add(double x, double y) => Console.WriteLine($"{x} + {y} = {x + y}");
+            void Sub(double x, double y) => Console.WriteLine($"{x} - {y} = {x - y}");
+            void Mul(double x, double y) => Console.WriteLine($"{x} * {y} = {x * y}");
+            void Div(double x, double y) => Console.WriteLine($"{x} / {y} = {x / y}");
+
             Console.Write("Введiть перше число: ");
             double a = Convert.ToInt32(Console.ReadLine());
 
@@ -20,27 +28,26 @@ namespace Calculator
             switch (x)
             {
                 case "+":
-                    Delegat Add = (c, d) => c + d;
-                    double del1 = Add(a, b);
-                    Console.WriteLine("Результат: {0}", del1);
+                    DoCalculation(a, b, Add);
                     break;
 
                 case "-":
-                    Delegat Sub = (c, d) => c - d;
-                    double del2 = Sub(a, b);
-                    Console.WriteLine("Результат: {0}", del2);
+                    DoCalculation(a, b, Sub);
                     break;
 
                 case "*":
-                    Delegat Mul = (c, d) => c * d;
-                    double del3 = Mul(a, b);
-                    Console.WriteLine("Результат: {0}", del3);
+                    DoCalculation(a, b, Mul);
                     break;
 
-                case "/":     
-                    Delegat Div = (c, d) => d == 0 ? 0 : c / d;
-                    double del4 = Div(a, b);
-                    Console.WriteLine("Результат: {0}", del4);
+                case "/":
+                    if (b == 0)
+                    {
+                        Console.WriteLine("На нуль дiлити не можна!");
+                    }
+                    else
+                    {
+                        DoCalculation(a, b, Div);
+                    }
                     break;
 
                 default:
